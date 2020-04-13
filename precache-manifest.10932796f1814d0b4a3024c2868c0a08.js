555b8f205bb4f50a687bb7d927616b082d0fe6cc
System Administrator <root@Les-MacBook-Air.local>
2020-04-13 15:30:23 +0700

    Updates

---
+++ precache-manifest.10932796f1814d0b4a3024c2868c0a08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e77e93c0a7ac729a70b2a8df98138063",
+    "url": "/my-reactjs/index.html"
+  },
+  {
+    "revision": "dd8b82fbc406e176cdc7",
+    "url": "/my-reactjs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e80eaebc7f8dc0103d92",
+    "url": "/my-reactjs/static/js/2.1e1d7b02.chunk.js"
+  },
+  {
+    "revision": "d9258a3aa7719dfca6ccef8faeb914c3",
+    "url": "/my-reactjs/static/js/2.1e1d7b02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd8b82fbc406e176cdc7",
+    "url": "/my-reactjs/static/js/main.5c0b3825.chunk.js"
+  },
+  {
+    "revision": "daedcab62ecbbd65982d",
+    "url": "/my-reactjs/static/js/runtime-main.b59e9604.js"
+  }
+]);
